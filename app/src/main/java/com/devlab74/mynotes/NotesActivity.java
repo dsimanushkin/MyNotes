eea1980ee098bf959e166aab1cab6f13db128284
dsimanushkin <dsimanushkin74@gmail.com>
2019-05-24 13:06:00 -0400

    Basic configurations

--- app/src/main/java/com/devlab74/mynotes/NotesActivity.java
+++ app/src/main/java/com/devlab74/mynotes/NotesActivity.java
@@ -1,14 +1,12 @@
 package com.devlab74.mynotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NotesActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes);
     }
 }
